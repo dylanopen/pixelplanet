68ee2e7239cf5a8fa1565fb6fed7cd5e41b67914
Dylan Rogers <opendylan@proton.me>
2025-12-12 23:40:01 +0000

    add sunlight

--- src/light.rs
+++ src/light.rs
@@ -1,8 +1,7 @@
+use std::f32::consts::PI;
+
 use bevy::{
-    app::{Plugin, Startup},
-    ecs::system::Commands,
-    light::AmbientLight,
-    log::info,
+    app::{Plugin, Startup}, ecs::system::Commands, light::{AmbientLight, CascadeShadowConfigBuilder, DirectionalLight, light_consts}, log::info, math::{Quat, Vec3}, transform::components::Transform, utils::default
 };
 
 pub struct LightPlugin;
@@ -10,7 +9,7 @@ pub struct LightPlugin;
 impl Plugin for LightPlugin {
     fn build(&self, app: &mut bevy::app::App) {
         app.add_systems(Startup, spawn_ambient_light);
-        //app.add_systems(Startup, spawn_sun_light);
+        app.add_systems(Startup, spawn_sun_light);
     }
 }
 
@@ -18,8 +17,30 @@ fn spawn_ambient_light(mut commands: Commands) {
     info!("Spawning ambient light");
     commands.insert_resource(AmbientLight {
         color: bevy::color::Color::WHITE,
-        brightness: 800.0,
+        brightness: 100.0,
         affects_lightmapped_meshes: false,
     });
+}
+
+fn spawn_sun_light(mut commands: Commands) {
+    commands.spawn((
+            DirectionalLight {
+                illuminance: light_consts::lux::OVERCAST_DAY,
+                shadows_enabled: true,
+                ..default()
+            },
+            Transform {
+                translation: Vec3::new(-10.0, 4.0, -5.0),
+                rotation: Quat::from_rotation_x(-PI / 4.),
+                ..default()
+            },
+            CascadeShadowConfigBuilder {
+                first_cascade_far_bound: 4.0,
+                maximum_distance: 10.0,
+                ..default()
+            }
+            .build(),
+    ));
+
 }
 
