4b8270b83595a9a2e6237009f746cb92ad614b11
Dylan Rogers <opendylan@proton.me>
2025-12-13 23:56:28 +0000

    components/tile_selected_indicator: fix errors/warnings

--- src/components/tile_selected_indicator.rs
+++ src/components/tile_selected_indicator.rs
@@ -1,3 +1,8 @@
+use bevy::ecs::component::Component;
+use bevy::mesh::{Mesh3d};
+use bevy::pbr::{MeshMaterial3d, StandardMaterial};
+use bevy::transform::components::Transform;
+
 #[derive(Component)]
 #[require(Mesh3d, MeshMaterial3d<StandardMaterial>, Transform)]
 pub struct TileSelectedIndicator;
