1daf1aef9b201ec554cae78f90618da887a0b3b4
Dylan Rogers <opendylan@proton.me>
2025-12-13 09:28:11 +0000

    basic voxel terrain generation

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,14 @@ use bevy::{
     DefaultPlugins,
     app::{App, Startup},
 };
-use bevy_map_camera::MapCameraPlugin;
 
 fn main() {
     let mut app = App::new();
     app.add_plugins(DefaultPlugins);
-    app.add_plugins(MapCameraPlugin);
+    app.add_plugins(bevy_map_camera::MapCameraPlugin);
     app.add_plugins(pixelplanet::camera::CameraPlugin);
     app.add_plugins(pixelplanet::light::LightPlugin);
+    app.add_plugins(pixelplanet::terrain::TerrainPlugin);
     app.add_systems(Startup, temp::spawn_example_cube);
     app.run();
 }
---
+++ src/terrain.rs
@@ -0,0 +1,38 @@
+use bevy::{app::{App, Plugin, Startup}, asset::Assets, color::Color, ecs::{component::Component, system::{Commands, ResMut}}, math::primitives::Cuboid, mesh::{Mesh, Mesh3d}, pbr::{MeshMaterial3d, StandardMaterial}, transform::components::Transform};
+
+pub struct TerrainPlugin;
+
+impl Plugin for TerrainPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, spawn_terrain_voxels);
+    }
+}
+
+#[derive(Component)]
+pub struct TerrainVoxel;
+
+fn spawn_grass_voxel(
+    commands: &mut Commands,
+    meshes: &mut ResMut<Assets<Mesh>>,
+    materials: &mut ResMut<Assets<StandardMaterial>>,
+    transform: Transform,
+) {
+    commands.spawn((
+        TerrainVoxel,
+        Mesh3d(meshes.add(Cuboid::new(1.0, 0.875, 1.0))),
+        MeshMaterial3d(materials.add(Color::linear_rgb(0.3, 0.7, 0.3))),
+        transform,
+    ));
+}
+
+fn spawn_terrain_voxels(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    for x in -20..20 {
+        for z in -20..20 {
+            spawn_grass_voxel(&mut commands, &mut meshes, &mut materials, Transform::from_xyz(x as f32, 0.0, z as f32));
+        }
+    }
+}
