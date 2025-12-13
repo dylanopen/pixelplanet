f6951e2b162bd897cb27cbb514e778ffd939deb6
Dylan Rogers <opendylan@proton.me>
2025-12-13 23:58:49 +0000

    spawn/main_camera: fix errors/warnings

--- src/spawn/main_camera.rs
+++ src/spawn/main_camera.rs
@@ -1,3 +1,9 @@
+use bevy::render::view::Msaa;
+use bevy_map_camera::MapCamera;
+use bevy_mesh_outline::OutlineCamera;
+
+use crate::components::main_camera::MainCamera;
+
 pub fn spawn_camera(mut commands: bevy::ecs::system::Commands) {
     commands.spawn((MapCamera, MainCamera, Msaa::Off, OutlineCamera));
 }
