232ed3a1b047f53ce8b3b4662e8f7060420b1e5d
Dylan Rogers <opendylan@proton.me>
2025-12-14 16:20:27 +0000

    create consts::IVEC2_UP

--- src/consts/mod.rs
+++ src/consts/mod.rs
@@ -2,3 +2,5 @@ use bevy::math::{IVec2, Vec3};
 
 pub const TILEMAP_SIZE: IVec2 = IVec2::new(64, 64);
 pub const MODEL_SIZE: Vec3 = Vec3::splat(64.0);
+
+pub const IVEC2_UP: IVec2 = IVec2::new(0, 1);
