6abbba9d7ab98bffd5e56f0f2a0e5b115fefd02b
Dylan Rogers <opendylan@proton.me>
2025-12-14 09:29:17 +0000

    re-export components

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -2,3 +2,9 @@ pub mod ground;
 pub mod main_camera;
 pub mod terrain_voxel;
 pub mod tile_selected_indicator;
+
+pub use ground::Ground;
+pub use main_camera::MainCamera;
+pub use terrain_voxel::TerrainVoxel;
+pub use tile_selected_indicator::TileSelectedIndicator;
+
