b5bd0bb8aaa30d4d5be18eadc66d9ebaa4ffb8da
Dylan Rogers <opendylan@proton.me>
2025-12-14 16:20:50 +0000

    create consts::IVEC2_LEFT

--- src/consts/mod.rs
+++ src/consts/mod.rs
@@ -5,3 +5,4 @@ pub const MODEL_SIZE: Vec3 = Vec3::splat(64.0);
 
 pub const IVEC2_UP: IVec2 = IVec2::new(0, 1);
 pub const IVEC2_DOWN: IVec2 = IVec2::new(0, -1);
+pub const IVEC2_LEFT: IVec2 = IVec2::new(-1, 0);
