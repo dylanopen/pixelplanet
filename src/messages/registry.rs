4e6d8afee5d89aaba387421cdcb03bc03e49b7ca
Dylan Rogers <opendylan@proton.me>
2025-12-14 10:56:52 +0000

    register UpdateTileMessage

--- src/messages/registry.rs
+++ src/messages/registry.rs
@@ -5,4 +5,5 @@ use crate::messages;
 pub fn register_messages(app: &mut App) {
     app.add_message::<messages::ClickTileMessage>();
     app.add_message::<messages::CursorTileHoverMessage>();
+    app.add_message::<messages::UpdateTileMessage>();
 }
