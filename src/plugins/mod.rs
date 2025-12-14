4b6fac019cfa13171374fecbeb5aa8e675a4e1a0
Dylan Rogers <opendylan@proton.me>
2025-12-14 09:31:03 +0000

    re-export plugins

--- src/plugins/mod.rs
+++ src/plugins/mod.rs
@@ -2,3 +2,8 @@ pub mod camera;
 pub mod light;
 pub mod selector;
 pub mod terrain;
+
+pub use camera::CameraPlugin;
+pub use light::LightPlugin;
+pub use selector::SelectorPlugin;
+pub use terrain::TerrainPlugin;
