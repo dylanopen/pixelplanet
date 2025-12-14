948f5e8ee43ee098bb97892f02969279ec0ad012
Dylan Rogers <opendylan@proton.me>
2025-12-14 16:20:37 +0000

    create consts::IVEC2_DOWN

--- src/consts/mod.rs
+++ src/consts/mod.rs
@@ -4,3 +4,4 @@ pub const TILEMAP_SIZE: IVec2 = IVec2::new(64, 64);
 pub const MODEL_SIZE: Vec3 = Vec3::splat(64.0);
 
 pub const IVEC2_UP: IVec2 = IVec2::new(0, 1);
+pub const IVEC2_DOWN: IVec2 = IVec2::new(0, -1);
