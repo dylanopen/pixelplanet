6970cc32c3c8771f0e230db36aed868abe692ec8
Dylan Rogers <opendylan@proton.me>
2025-12-15 20:26:16 +0000

    reformat lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
+pub mod messages;
 pub mod components;
 pub mod consts;
+pub mod resources;
+
 pub mod executors;
 pub mod triggers;
-pub mod messages;
-pub mod plugins;
-pub mod resources;
 pub mod spawn;
+
+pub mod plugins;
