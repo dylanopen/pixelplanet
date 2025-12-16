19906939ce0ce2229b8d70c8757d37d95d8b7a2e
dylancode <opendylan@proton.me>
2025-12-16 12:17:25 +0000

    register forwarders::handle_purchase_tile

--- src/registry/forwarders.rs
+++ src/registry/forwarders.rs
@@ -4,4 +4,5 @@ use crate::forwarders;
 
 pub fn register_forwarders(app: &mut App) {
     app.add_systems(Update, forwarders::place_road);
+    app.add_systems(Update, forwarders::handle_purchase_tile);
 }
