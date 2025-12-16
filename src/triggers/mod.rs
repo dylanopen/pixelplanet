c30977f8f22a42c1e75723df802190df4f804aea
dylancode <opendylan@proton.me>
2025-12-16 12:19:29 +0000

    make triggers mods private; re-export functions of triggers

--- src/triggers/mod.rs
+++ src/triggers/mod.rs
@@ -1,3 +1,7 @@
-pub mod left_click;
-pub mod map_cursor_hover;
-pub mod tick_money_increase;
+mod left_click;
+mod map_cursor_hover;
+mod tick_money_increase;
+
+pub use left_click::on_left_click;
+pub use map_cursor_hover::map_cursor_hover;
+pub use tick_money_increase::tick_money_increase;
