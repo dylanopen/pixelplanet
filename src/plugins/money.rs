9b222f1061e8a8b017f8b9d5a17683222183f38d
dylancode <opendylan@proton.me>
2025-12-16 11:30:44 +0000

    register systems in MoneyPlugin

--- src/plugins/money.rs
+++ src/plugins/money.rs
@@ -2,6 +2,8 @@ use bevy::app::{App, Plugin, Startup, Update};
 
 use crate::executors::set_money::set_money_res;
 use crate::executors::update_money_display::update_money_display;
+use crate::forwarders::add_money::set_added_money;
+use crate::forwarders::purchase_plot::handle_purchase_tile;
 use crate::spawn::spawn_money_display;
 use crate::triggers::tick_money_increase::tick_money_increase;
 
@@ -13,5 +15,7 @@ impl Plugin for MoneyPlugin {
         app.add_systems(Update, tick_money_increase);
         app.add_systems(Update, set_money_res);
         app.add_systems(Update, update_money_display);
+        app.add_systems(Update, handle_purchase_tile);
+        app.add_systems(Update, set_added_money);
     }
 }
