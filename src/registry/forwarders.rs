fd18838e90cb11cf98f1f6a4426f55bc5dc4dc3e
dylancode <opendylan@proton.me>
2025-12-16 12:16:17 +0000

    create registry::forwarders

---
+++ src/registry/forwarders.rs
@@ -0,0 +1,5 @@
+use bevy::app::App;
+
+pub fn register_forwarders(app: &mut App) {
+
+}
--- src/registry/mod.rs
+++ src/registry/mod.rs
@@ -2,6 +2,7 @@ mod messages;
 mod resources;
 mod startups;
 mod executors;
+mod forwarders;
 
 use bevy::app::App;
 
@@ -10,6 +11,7 @@ pub fn register(mut app: App) -> App {
     resources::register_resources(&mut app);
     startups::register_startups(&mut app);
     executors::register_executors(&mut app);
+    forwarders::register_forwarders(&mut app);
 
     app
 }
