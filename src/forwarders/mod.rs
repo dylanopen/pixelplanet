bdf7a2531f1362f3a9e8135aa4f7890f658f4d92
dylancode <opendylan@proton.me>
2025-12-16 12:15:12 +0000

    make forwarders mods private; re-export functions of forwarders mods

--- src/forwarders/mod.rs
+++ src/forwarders/mod.rs
@@ -1,3 +1,8 @@
-pub mod add_money;
-pub mod place_road;
-pub mod purchase_plot;
+mod add_money;
+mod place_road;
+mod purchase_plot;
+
+pub use add_money::set_added_money;
+pub use place_road::place_road;
+pub use purchase_plot::handle_purchase_tile;
+
