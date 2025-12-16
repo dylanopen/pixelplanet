7e8b8f861a41da8c645a74408eb43805d941a1c1
dylancode <opendylan@proton.me>
2025-12-16 11:51:57 +0000

    move spawn_terrain_voxels to registry

--- src/plugins/terrain.rs
+++ src/plugins/terrain.rs
@@ -6,7 +6,6 @@ pub struct TerrainPlugin;
 
 impl Plugin for TerrainPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, spawn_terrain_voxels);
         app.add_systems(Startup, spawn_ground_plane);
     }
 }
--- src/registry/startups.rs
+++ src/registry/startups.rs
@@ -1,11 +1,12 @@
 use bevy::app::{App, Startup};
 
-use crate::spawn::{configure_camera_controls, spawn_main_camera, spawn_money_display, spawn_selector_mesh};
+use crate::spawn::{configure_camera_controls, spawn_main_camera, spawn_money_display, spawn_selector_mesh, spawn_terrain_voxels};
 
 pub fn register_startups(app: &mut App) {
     app.add_systems(Startup, spawn_main_camera);
     app.add_systems(Startup, configure_camera_controls);
     app.add_systems(Startup, spawn_money_display);
     app.add_systems(Startup, spawn_selector_mesh);
+    app.add_systems(Startup, spawn_terrain_voxels);
 
 }
