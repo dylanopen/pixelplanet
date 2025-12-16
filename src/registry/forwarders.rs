6e58cd8df5639ad94998e31188802d98563e5728
dylancode <opendylan@proton.me>
2025-12-16 12:17:12 +0000

    register forwarders::place_road

--- src/registry/forwarders.rs
+++ src/registry/forwarders.rs
@@ -1,5 +1,7 @@
-use bevy::app::App;
+use bevy::app::{App, Update};
+
+use crate::forwarders;
 
 pub fn register_forwarders(app: &mut App) {
-
+    app.add_systems(Update, forwarders::place_road);
 }
