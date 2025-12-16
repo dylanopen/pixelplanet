05c5b203c1d807fbafd97d13facb3e00704948d7
dylancode <opendylan@proton.me>
2025-12-16 11:34:10 +0000

    organise lib.rs into data and systems

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,13 @@
+// data
 pub mod components;
 pub mod consts;
 pub mod messages;
 pub mod resources;
 
-pub mod executors;
-pub mod forwarders;
+// systems
 pub mod triggers;
-
+pub mod forwarders;
+pub mod executors;
 pub mod spawn;
 
 pub mod plugins;
