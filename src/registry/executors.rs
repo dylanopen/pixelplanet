e58b7ad46236d6ae286a7ce386f004d232112fab
dylancode <opendylan@proton.me>
2025-12-16 12:04:37 +0000

    register executors::set_money_res

--- src/registry/executors.rs
+++ src/registry/executors.rs
@@ -5,4 +5,5 @@ use crate::executors;
 pub fn register_executors(app: &mut App) {
     app.add_systems(Update, executors::update_current_hovered_tile);
     app.add_systems(Update, executors::update_tile_selected_indicator);
+    app.add_systems(Update, executors::set_money_res);
 }
