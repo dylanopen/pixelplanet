b7d770fec87959e97fd97fc64131ff2b4a9f459f
dylancode <opendylan@proton.me>
2025-12-16 12:03:11 +0000

    make mods in executors private; public re-export their functions

--- src/executors/mod.rs
+++ src/executors/mod.rs
@@ -1,6 +1,14 @@
-pub mod cursor_tile_hover;
-pub mod set_money;
-pub mod set_tile;
-pub mod update_money_display;
-pub mod update_tile;
-pub mod update_tile_model;
+mod cursor_tile_hover;
+mod set_money;
+mod set_tile;
+mod update_money_display;
+mod update_tile;
+mod update_tile_model;
+
+pub use cursor_tile_hover::update_current_hovered_tile;
+pub use cursor_tile_hover::update_tile_selected_indicator;
+pub use set_money::set_money_res;
+pub use set_tile::set_tile_in_tilemap;
+pub use update_money_display::update_money_display;
+pub use update_tile::update_tiles;
+pub use update_tile_model::update_tile_models;
