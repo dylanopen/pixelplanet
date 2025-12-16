3327bdba518f4df47c0e380f9e830a222665c6ce
dylancode <opendylan@proton.me>
2025-12-16 12:21:19 +0000

    create registry::triggers

--- src/registry/mod.rs
+++ src/registry/mod.rs
@@ -1,8 +1,9 @@
 mod messages;
 mod resources;
 mod startups;
-mod executors;
+mod triggers;
 mod forwarders;
+mod executors;
 
 use bevy::app::App;
 
@@ -10,8 +11,9 @@ pub fn register(mut app: App) -> App {
     messages::register_messages(&mut app);
     resources::register_resources(&mut app);
     startups::register_startups(&mut app);
-    executors::register_executors(&mut app);
+    triggers::register_triggers(&mut app);
     forwarders::register_forwarders(&mut app);
+    executors::register_executors(&mut app);
 
     app
 }
---
+++ src/registry/triggers.rs
@@ -0,0 +1,6 @@
+use bevy::app::{App, Update};
+
+use crate::triggers;
+
+pub fn register_triggers(app: &mut App) {
+}
