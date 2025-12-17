dd78d4bc772263b9d613ccdcd1f9b2c0e7b0f4be
Dylan Rogers <opendylan@proton.me>
2025-12-17 21:03:45 +0000

    register triggers::change_toolbar_tool

--- src/registry/triggers.rs
+++ src/registry/triggers.rs
@@ -6,4 +6,5 @@ pub fn register_triggers(app: &mut App) {
     app.add_systems(Update, triggers::on_left_click);
     app.add_systems(Update, triggers::map_cursor_hover);
     app.add_systems(Update, triggers::tick_money_increase);
+    app.add_systems(Update, triggers::change_toolbar_tool);
 }
