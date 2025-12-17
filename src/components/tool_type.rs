7065d652cbde74148156efa84f2aed23e77259fa
Dylan Rogers <opendylan@proton.me>
2025-12-17 20:55:14 +0000

    derive Debug and Clone for ToolType

--- src/components/tool_type.rs
+++ src/components/tool_type.rs
@@ -2,7 +2,7 @@ use bevy::ecs::component::Component;
 
 use crate::components::TileType;
 
-#[derive(Component)]
+#[derive(Component, Debug, Clone)]
 pub enum ToolType {
     Select,
     PlaceTile(TileType),
