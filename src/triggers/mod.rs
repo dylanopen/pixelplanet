ff03c5d79d9cdcecbcc8bbd1b4cc106971421957
Dylan Rogers <opendylan@proton.me>
2025-12-17 20:33:37 +0000

    create trigger change_toolbar_tool

--- src/triggers/mod.rs
+++ src/triggers/mod.rs
@@ -1,7 +1,9 @@
 mod left_click;
 mod map_cursor_hover;
 mod tick_money_increase;
+mod toolbar_change;
 
 pub use left_click::on_left_click;
 pub use map_cursor_hover::map_cursor_hover;
 pub use tick_money_increase::tick_money_increase;
+pub use toolbar_change::change_toolbar_tool;
---
+++ src/triggers/toolbar_change.rs
@@ -0,0 +1,31 @@
+use bevy::ecs::message::MessageWriter;
+use bevy::input::{ButtonInput, keyboard::KeyCode};
+use bevy::ecs::system::Res;
+
+use crate::messages::ChangeToolbarSlotMessage;
+
+pub fn change_toolbar_tool(
+    keys: Res<ButtonInput<KeyCode>>,
+    mut change_toolbar_slot_mw: MessageWriter<ChangeToolbarSlotMessage>,
+) {
+    for key in keys.get_just_pressed() {
+        let slot_index = match key {
+            KeyCode::Digit1 => Some(1),
+            KeyCode::Digit2 => Some(2),
+            KeyCode::Digit3 => Some(3),
+            KeyCode::Digit4 => Some(4),
+            KeyCode::Digit5 => Some(5),
+            KeyCode::Digit6 => Some(6),
+            KeyCode::Digit7 => Some(7),
+            KeyCode::Digit8 => Some(8),
+            KeyCode::Digit9 => Some(9),
+            KeyCode::Digit0 => Some(0),
+            _ => None,
+        };
+        if let Some(index) = slot_index {
+            change_toolbar_slot_mw.write(ChangeToolbarSlotMessage {
+                slot_index: index,
+            });
+        }
+    }
+}
