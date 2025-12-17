9703ab840b62a54534ffb4fe388a7573467f2584
dylancode <opendylan@proton.me>
2025-12-17 14:08:26 +0000

    create component BuildButton

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,13 +1,18 @@
+pub mod tile;
+pub mod ui;
+
 pub mod ground;
 pub mod main_camera;
 pub mod money_display;
 pub mod terrain_voxel;
-pub mod tile;
 pub mod tile_selected_indicator;
+
+
+pub use tile::*;
+pub use ui::*;
 
 pub use ground::Ground;
 pub use main_camera::MainCamera;
 pub use money_display::MoneyDisplay;
 pub use terrain_voxel::TerrainVoxel;
-pub use tile::*;
 pub use tile_selected_indicator::TileSelectedIndicator;
---
+++ src/components/ui/mod.rs
@@ -0,0 +1,4 @@
+pub mod build_button;
+
+pub use build_button::BuildButton;
+
