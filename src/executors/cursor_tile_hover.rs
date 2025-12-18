fd6434a816564f3477a200b1edd03ca658cd87d4
dylancode <opendylan@proton.me>
2025-12-18 11:51:44 +0000

    fix: hide selector if hovered tile not empty

--- src/executors/cursor_tile_hover.rs
+++ src/executors/cursor_tile_hover.rs
@@ -3,7 +3,7 @@ use bevy::{
     ecs::{
         message::MessageReader,
         query::With,
-        system::{ResMut, Single},
+        system::{Res, ResMut, Single},
     },
     math::Vec3,
     transform::components::Transform,
@@ -11,19 +11,24 @@ use bevy::{
 
 use crate::{
     components::TileSelectedIndicator, messages::CursorTileHoverMessage,
-    resources::CurrentHoveredTile,
+    resources::{CurrentHoveredTile, Tilemap},
 };
 
 pub fn update_tile_selected_indicator(
     mut indicator_query: Single<(&mut Transform, &mut Visibility), With<TileSelectedIndicator>>,
     mut hover_mr: MessageReader<CursorTileHoverMessage>,
+    tilemap: Res<Tilemap>
 ) {
     for hover_msg in hover_mr.read() {
         match hover_msg.pos {
             Some(pos) => {
-                indicator_query.0.translation =
-                    Vec3::new(pos.x as f32 + 0.5, 1.52, pos.y as f32 + 0.5);
-                *indicator_query.1 = Visibility::Visible;
+                if tilemap.is_empty(pos) {
+                    *indicator_query.1 = Visibility::Hidden
+                } else {
+                    indicator_query.0.translation =
+                        Vec3::new(pos.x as f32 + 0.5, 1.0, pos.y as f32 + 0.5);
+                    *indicator_query.1 = Visibility::Visible;
+                }
             }
             None => {
                 *indicator_query.1 = Visibility::Hidden;
