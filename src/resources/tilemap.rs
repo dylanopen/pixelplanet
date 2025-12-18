c96fcea6a18dfb0c0380958ad541cc074a4519e5
dylancode <opendylan@proton.me>
2025-12-18 11:19:33 +0000

    create Tilemap::get_straight_neighbor_positions

--- src/resources/tilemap.rs
+++ src/resources/tilemap.rs
@@ -146,6 +146,24 @@ impl Tilemap {
             update_tile_mw.write(UpdateTileMessage { pos: neighbor_pos });
         }
     }
+
+    pub fn get_straight_neighbor_positions(&self, pos: IVec2) -> Vec<IVec2> {
+        let mut neighbors = Vec::new();
+        let directions = [
+            IVec2::new(0, 1),
+            IVec2::new(1, 0),
+            IVec2::new(0, -1),
+            IVec2::new(-1, 0),
+        ];
+
+        for dir in directions.iter() {
+            let neighbor_pos = pos + *dir;
+            if self.within_range(neighbor_pos) {
+                neighbors.push(neighbor_pos);
+            }
+        }
+        neighbors
+    }
 }
 
 impl Default for Tilemap {
