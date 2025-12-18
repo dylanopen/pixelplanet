03bd0209d678b0a30ef8d88b887aa3419c60492c
dylancode <opendylan@proton.me>
2025-12-18 11:13:52 +0000

    update place_tool_tile to check can_place_at

--- src/forwarders/place_tool_tile.rs
+++ src/forwarders/place_tool_tile.rs
@@ -6,13 +6,14 @@ use bevy::ecs::{
 use crate::{
     components::ToolType,
     messages::{ClickTileMessage, PurchaseTileMessage},
-    resources::ActiveToolType,
+    resources::{ActiveToolType, Tilemap},
 };
 
 pub fn place_tool_tile(
     mut click_tile_mr: MessageReader<ClickTileMessage>,
     mut purchase_tile_mw: MessageWriter<PurchaseTileMessage>,
     active_tool_type: Res<ActiveToolType>,
+    tilemap: Res<Tilemap>,
 ) {
     let tool_type = &active_tool_type.0;
     let ToolType::PlaceTile(tiletype) = tool_type else {
@@ -20,6 +21,9 @@ pub fn place_tool_tile(
     };
     for msg in click_tile_mr.read() {
         let pos = msg.pos;
+        if !tiletype.can_place_at(pos, &tilemap) {
+            continue;
+        }
 
         purchase_tile_mw.write(PurchaseTileMessage {
             pos,
