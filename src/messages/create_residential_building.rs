c6029bd70524110127798aa90435ecf879339e93
Dylan Rogers <opendylan@proton.me>
2025-12-20 00:34:39 +0000

    add field residents to CreateResidentialBuildingMessage

--- src/messages/create_residential_building.rs
+++ src/messages/create_residential_building.rs
@@ -4,5 +4,6 @@ use bevy::ecs::message::Message;
 #[derive(Debug, Clone, Message)]
 pub struct CreateResidentialBuildingMessage {
     pub pos: IVec2,
+    pub residents: u32,
     pub capacity: u32,
 }
