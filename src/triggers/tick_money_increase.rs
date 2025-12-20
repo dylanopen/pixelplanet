266e7d74683dc9de6baf9d1459c6e710d0f67316
Dylan Rogers <opendylan@proton.me>
2025-12-20 23:05:16 +0000

    update trigger tick_money_increase to pay taxes

--- src/triggers/tick_money_increase.rs
+++ src/triggers/tick_money_increase.rs
@@ -1,9 +1,20 @@
+use crate::components::ResidentialBuilding;
 use crate::messages::AddMoneyMessage;
+use crate::resources::ResidentialTax;
 use bevy::ecs::message::MessageWriter;
-use bevy::ecs::system::Res;
+use bevy::ecs::system::{Query, Res};
 use bevy::time::Time;
 
-pub fn tick_money_increase(time: Res<Time>, mut set_money_mw: MessageWriter<AddMoneyMessage>) {
-    let increase = time.delta_secs_f64() * 5.0; // increase money by 5 per second
-    set_money_mw.write(AddMoneyMessage { increase });
+pub fn tick_money_increase(
+    time: Res<Time>,
+    mut set_money_mw: MessageWriter<AddMoneyMessage>,
+    residential_buildings: Query<&ResidentialBuilding>,
+    residential_tax: Res<ResidentialTax>,
+) {
+    for building in residential_buildings.iter() {
+        let tax = residential_tax.rate;
+        let residents = building.residents as f64;
+        let increase = time.delta_secs_f64() * residents * tax;
+        set_money_mw.write(AddMoneyMessage { increase });
+    }
 }
