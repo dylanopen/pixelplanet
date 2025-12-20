7585137bc9f09f4bd268038cf71fb65e80b08bb2
Dylan Rogers <opendylan@proton.me>
2025-12-20 00:29:58 +0000

    create ResidentialVariant::get_capacity

--- src/components/tile/residential_variant.rs
+++ src/components/tile/residential_variant.rs
@@ -5,4 +5,8 @@ impl ResidentialVariant {
     pub fn get_model_name(&self) -> String {
         format!("house{}", self.0)
     }
+
+    pub fn get_capacity(&self) -> u32 {
+        50 // in the future, this will vary based on the residential variant.
+    }
 }
