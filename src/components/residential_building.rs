7546a847e8676e8e1753057b2ba7c266aa6b5655
Dylan Rogers <opendylan@proton.me>
2025-12-20 00:34:09 +0000

    derive Component for ResidentialBuilding

--- src/components/residential_building.rs
+++ src/components/residential_building.rs
@@ -1,6 +1,6 @@
-use bevy::math::IVec2;
+use bevy::{ecs::component::Component, math::IVec2};
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Component)]
 pub struct ResidentialBuilding {
     pub pos: IVec2,
     pub residents: u32,
